eb8aa8760995abacf2acf4f154fb1e993cd4d762
nachkonvention <j.lohmueller@delinero.de>
2013-08-22 11:24:15 +0200

    murks removed

--- src/log/Logger.java
+++ src/log/Logger.java
@@ -1,22 +1,5 @@
 package log;
 
-//TODO: Remove ASCII-Batman
-/*
-
-       _==/          i     i          \==_
-     /XX/            |\___/|            \XX\
-   /XXXX\            |XXXXX|            /XXXX\
-  |XXXXXX\_         _XXXXXXX_         _/XXXXXX|
- XXXXXXXXXXXxxxxxxxXXXXXXXXXXXxxxxxxxXXXXXXXXXXX
-|XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-|XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|
- XXXXXX/^^^^"\XXXXXXXXXXXXXXXXXXXXX/^^^^^\XXXXXX
-  |XXX|       \XXX/^^\XXXXX/^^\XXX/       |XXX|
-    \XX\       \X/    \XXX/    \X/       /XX/
-       "\       "      \X/      "      /"
- */
-
 import gui.console.ConsolePanel;
 
 public class Logger {
