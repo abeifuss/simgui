056e94cc6f63ad4c407de0e9848c7a9110a69c1d
nachkonvention <j.lohmueller@googlemail.com>
2013-10-30 00:35:20 +0100

    NEW - task 36: Anordnung der Accordion-Elemente festlegen http://knobipresse.no-ip.biz/issues/show/36

--- src/evaluation/simulator/gui/customElements/accordion/AccordionEntry.java
+++ src/evaluation/simulator/gui/customElements/accordion/AccordionEntry.java
@@ -5,6 +5,9 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.ImageIcon;
@@ -28,23 +31,14 @@ public class AccordionEntry extends JPanel {
 			final List<SimProp> listOfAllSectionsInACategory,
 			final JComboBox<String> jComboBox) {
 		this.localName = name;
+		System.err.println(this.localName);
 		this.setLayout(new BorderLayout(0, 0));
-
 		this.entryButton = new JButton(this.localName, new ImageIcon(
 				"etc/img/icons/green/arrow-144-24.png"));
 		this.entryButton.setForeground(Color.BLACK);
 		this.entryButton.setHorizontalAlignment(SwingConstants.LEFT);
 		this.entryButton.setHorizontalTextPosition(SwingConstants.RIGHT);
 
-		/*
-		 * // This takes care that the non-editable cells are grayed out.
-		 * 
-		 * @Override public Component prepareRenderer(TableCellRenderer
-		 * renderer, int row, int column) { Component c =
-		 * super.prepareRenderer(renderer, row, column); if ((column == 1) &&
-		 * !this.isCellEditable(row, column)) { c.setBackground(new Color(255,
-		 * 200, 200)); } return c; } }
-		 */
 		ActionListener actionListener = new ActionListener() {
 
 			@Override
@@ -58,10 +52,10 @@ public class AccordionEntry extends JPanel {
 
 		this.add(this.entryButton, BorderLayout.NORTH);
 		jComboBox.setVisible(false);
-		jComboBox.addActionListener(new ActionListener() {
+		jComboBox.addItemListener(new ItemListener() {
 
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void itemStateChanged(ItemEvent e) {
 				AccordionEntry.this.comboBoxChanged(jComboBox,
 						listOfAllSectionsInACategory);
 
@@ -77,6 +71,17 @@ public class AccordionEntry extends JPanel {
 	private void comboBoxChanged(JComboBox<String> jComboBox,
 			List<SimProp> listOfAllSectionsInACategory) {
 		if (jComboBox.getSelectedIndex() != 0) {
+			List<SimProp> tmpListOfAllSectionsInACategory = new ArrayList<SimProp>();
+			for (SimProp simProp : listOfAllSectionsInACategory) {
+				System.err.println(simProp.getNamespace());
+				if (simProp.getNamespace().equals(jComboBox.getSelectedItem())
+						&& (jComboBox.getSelectedIndex() != 0)) {
+					tmpListOfAllSectionsInACategory.add(simProp);
+				}
+			}
+			for (SimProp simProp : tmpListOfAllSectionsInACategory) {
+				System.out.println(simProp.getName());
+			}
 			this.entryTable = new JTable(new AccordionModel(this.localName)) {
 
 				// This takes care that the non-editable cells are grayed out.
@@ -93,11 +98,10 @@ public class AccordionEntry extends JPanel {
 
 			this.entryTable
 					.addMouseMotionListener(new AccordionMouseMotionAdapter(
-							listOfAllSectionsInACategory, this.entryTable));
-			this.entryTable
-					.setDefaultRenderer(Object.class,
-							new AccordionTableCellRenderer(
-									listOfAllSectionsInACategory));
+							tmpListOfAllSectionsInACategory, this.entryTable));
+			this.entryTable.setDefaultRenderer(Object.class,
+					new AccordionTableCellRenderer(
+							tmpListOfAllSectionsInACategory));
 			this.entryTable.setVisible(true);
 
 			TableColumn col = this.entryTable.getColumnModel().getColumn(1);
