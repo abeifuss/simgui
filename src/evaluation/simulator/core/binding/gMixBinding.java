d2de3a95b4d8e3bcb3ef2bc49aa4584ea4e69ff1
Alexander Beifuss <7beifuss@informatik.uni-hamburg.de>
2013-11-07 20:42:18 +0100

    result set is accessible from gmixbinding *trollversion*

--- src/evaluation/simulator/core/binding/gMixBinding.java
+++ src/evaluation/simulator/core/binding/gMixBinding.java
@@ -1,9 +1,11 @@
 package evaluation.simulator.core.binding;
 
 import evaluation.simulator.Simulator;
+import evaluation.simulator.core.statistics.ResultSet;
 import evaluation.simulator.core.statistics.Statistics;
 import evaluation.simulator.log.LogLevel;
 import evaluation.simulator.log.Logger;
+import evaluation.simulator.pluginRegistry.PlotType;
 import framework.core.launcher.CommandLineParameters;
 
 public class gMixBinding extends Thread {
@@ -20,9 +22,15 @@ public class gMixBinding extends Thread {
 		try {
 			Simulator.reset();
 			Simulator gMixSim = new Simulator(params);
-			Logger.Log(LogLevel.INFO, "Finished Simulator");
+			ResultSet results = gMixSim.results;
+			if (results != null){
+				Logger.Log(LogLevel.INFO, "Finished simulator with results");
+			}
 			
-			stats = Simulator.trafficSourceStatistics;
+			for ( int i = 0; i < 1337 ; i++){ // muhahah ich bin ein troll!!! :D
+				for (PlotType plotType:results.getDesiredPlotTypes())
+					plotType.plot(results);
+			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
