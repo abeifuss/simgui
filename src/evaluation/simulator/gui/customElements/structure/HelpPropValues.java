87bf91aacd3619bcd0fa570953272d211f25ccf8
Malte <7weinsch@informatik.uni-hamburg.de>
2014-01-10 00:49:52 +0100

    NEW - task 56: Properties to vary definierbar machen http://knobipresse.no-ip.biz/issues/show/56
    still debugging

--- src/evaluation/simulator/gui/customElements/structure/HelpPropValues.java
+++ src/evaluation/simulator/gui/customElements/structure/HelpPropValues.java
@@ -21,7 +21,7 @@ public class HelpPropValues {
 	
 	public HelpPropValues(String value, Class type){
 		
-		values = new LinkedList<>();
+		values = new LinkedList<String>();
 		this.type = type;
 		
 		tokenizer = new StringTokenizer( value, " " );
@@ -36,7 +36,11 @@ public class HelpPropValues {
 		
 		for(String s : values){
 			
-			if (this.type==Integer.class){
+			logger.log(Level.DEBUG, s);
+			logger.log(Level.DEBUG, type.toString());
+			logger.log(Level.DEBUG, Integer.class.toString());
+			
+			if (this.type == Integer.class){
 				try{
 					int i = Integer.parseInt(s);
 				}
@@ -46,7 +50,7 @@ public class HelpPropValues {
 				}
 			}
 			
-			if (this.type==Boolean.class){
+			if (this.type == Boolean.class){
 				try{
 					boolean b = Boolean.parseBoolean(s);
 				}
@@ -56,7 +60,7 @@ public class HelpPropValues {
 				}
 			}
 			
-			if (this.type==Float.class){
+			if (this.type == Float.class){
 				try{
 					float f = Float.parseFloat(s);
 				}
@@ -66,7 +70,7 @@ public class HelpPropValues {
 				}
 			}
 			
-			if (this.type==Double.class){
+			if (this.type == Double.class){
 				try{
 					double d = Double.parseDouble(s);
 				}
