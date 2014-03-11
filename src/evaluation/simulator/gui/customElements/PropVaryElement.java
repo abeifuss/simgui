efb52b507379163d99ddaef86cfaf33abf37282c
2langnic <2langnic@informatik.uni-hamburg.de>
2014-03-11 20:47:05 +0100

    added: properties to vary to unsaved changes

--- src/evaluation/simulator/gui/customElements/PropVaryElement.java
+++ src/evaluation/simulator/gui/customElements/PropVaryElement.java
@@ -178,6 +178,8 @@ public class PropVaryElement extends JPanel {
 
 		logger.log(Level.DEBUG, currentElement.getSelectedText());
 		this.repaint();
+		SimPropRegistry.getInstance().setUnsavedChanges(true);
+
 	}
 
 	private void addBoxListener(final JComboBox<String> box) {
@@ -243,6 +245,7 @@ public class PropVaryElement extends JPanel {
 				    "Invalid Value",
 				    JOptionPane.ERROR_MESSAGE);
 		}
+		SimPropRegistry.getInstance().setUnsavedChanges(true);
 	}
 
 	/**
@@ -277,6 +280,7 @@ public class PropVaryElement extends JPanel {
 		values = SimPropRegistry.getInstance().getPropertiesToVary().get("VALUES_FOR_THE_SECOND_PROPERTY_TO_VARY");
 		logger.log(Level.DEBUG, values);
 		propElement[1].setText(values);
+		
 	}
 
 }
